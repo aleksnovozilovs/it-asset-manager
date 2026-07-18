d1ece3ea72bc0cd103552afdbb6e7e570d3b65b3
Aleks Novozilovs <aleksandr.novozhilov@gmail.com>
2026-07-18 13:02:41 +0100

    Refactor menu into reusable functions

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 from device import Device
 
-devices = [
-    Device("Aleks laptop", "Apple", "M4", "24GB"), 
-    Device("Reception PC", "Dell", "Intel i5 Ultra", "16GB")
-]
+def show_menu():
+    print("===== IT Asset Manager =====")
+    print()
+    print("1. Show Devices")
+    print("2. Add Device")
+    print("3. Exit ")
+    print()
 
-print("===== IT Asset Manager =====")
-print()
-print("1. Show Devices")
-print("2. Add Device")
-print("3. Exit ")
-print()
-option = input("Choose an option: ")
-print()
-
-if option == "1":
+def show_devices(devices):
     for device in devices:
-        device.display_info()
-elif option == "2":
+            device.display_info()
+
+def add_device(devices):
     new_device = Device(input("Enter device name: "), input("Enter device brand: "),
                                                      input("Enter devices CPU: "),
                                                             input("Enter device RAM: "))
     devices.append(new_device)
     print()
     print("Device added: ")
+    print()
     new_device.display_info()
-    
-elif option == "3":
-    print("Good bye!")
-    print()
-    exit()
-else:
-    print("Incorrect option, please restart the programm and try again")
-    print()
+  
+devices = [
+    Device("Aleks laptop", "Apple", "M4", "24GB"), 
+    Device("Reception PC", "Dell", "Intel i5 Ultra", "16GB")
+]
+option = " "
+while option != "3":
+    show_menu()
+    option = input("Choose an option: ")
+    if option == "1":
+         show_devices(devices)
+    elif option == "2":
+        add_device(devices)
+    elif option == "3":
+        print("Goodbye!")
+        print()
+    else:
+        print("Incorrect option, please restart the programm and try again")
+        print()
